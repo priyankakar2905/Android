d11ca540b3719e30585fc7cc23931ff34be65b13
Priyanka K <priyanka99548@gmail.com>
2022-07-05 00:51:53 +0200

    New java file added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        String number1 = "31";
+        String number2 = "32";
+        System.out.println("Result :" +(Integer.parseInt(number1) + Integer.parseInt(number2)));
+    }
+}
